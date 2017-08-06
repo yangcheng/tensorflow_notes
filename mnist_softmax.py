fix double softmax
the induced unstable softmax reduced the accuracy

mnist_softmax.py
@@ -8,7 +8,7 @@ x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+y = tf.matmul(x, W) + b
 
 y_ = tf.placeholder(tf.float32, [None, 10])
 
@@ -20,10 +20,10 @@ train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 sess = tf.InteractiveSession()
 
-tf.global_variables_initializer().run()
+sess.run(tf.global_variables_initializer())
 
-for _ in range(2000):
-    batch_xs, batch_ys = mnist.train.next_batch(200)
+for _ in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))